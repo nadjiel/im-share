cfb4f1da6f7f1c6772a2cdf6d9b9b4da1548953b
Ulisses <iamurise@gmail.com>
2023-01-27 07:48:22 -0300

    remove aws

--- src/env.js
+++ src/env.js
@@ -1,12 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const {
-  AWS_ACCESS_KEY,
-  AWS_ACCESS_SECRET,
-
-  POSTGRES_DB,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  DATABASE_URL,
-} = process.env;
+export const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, DATABASE_URL } =
+  process.env;
