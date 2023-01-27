6461baf4f6538753194b88b02d6df614042dfacb
Ulisses <iamurise@gmail.com>
2023-01-27 07:57:13 -0300

    refactor get signature

--- src/controller/Upload.js
+++ src/controller/Upload.js
@@ -1,3 +1,4 @@
+import { v2 } from "cloudinary";
 import { clConfig } from "./clConfig";
 
 app.get("/get-signature", (req, res) => {
@@ -5,26 +6,16 @@ app.get("/get-signature", (req, res) => {
 });
 
 app.post("/do-something-with-photo", async (req, res) => {
-  // based on the public_id and the version that the (potentially malicious) user is submitting...
-  // we can combine those values along with our SECRET key to see what we would expect the signature to be if it was innocent / valid / actually coming from Cloudinary
-  const expectedSignature = cloudinary.utils.api_sign_request(
-    { public_id: req.body.public_id, version: req.body.version },
+  const { publicId, version } = req.body;
+
+  const expectedSignature = v2.utils.api_sign_request(
+    { public_id: publicId, version },
     clConfig.api_secret
   );
 
-  // We can trust the visitor's data if their signature is what we'd expect it to be...
-  // Because without the SECRET key there's no way for someone to know what the signature should be...
-  if (expectedSignature === req.body.signature) {
-    // Do whatever you need to do with the public_id for the photo
-    // Store it in a database or pass it to another service etc...
-    await fse.ensureFile("./data.txt");
-    const existingData = await fse.readFile("./data.txt", "utf8");
-    await fse.outputFile(
-      "./data.txt",
-      existingData + req.body.public_id + "\n"
-    );
+  if (expectedSignature !== req.body.signature) {
+    res.status(403).json({ error: "The signature is invalid" });
   }
-});
 
-// get url + cloudinaryConfig.cloud_name + id
-// delete cloudinary.uploader.destroy(id);
+  // todo save using publicId
+});
