1e22236651d237eac52b48ec53c6cfdb4bed6df5
Ulisses <iamurise@gmail.com>
2023-01-31 08:06:40 -0300

    rename is authenticated to auth middleware

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -3,7 +3,6 @@ import { userRouter } from "./user/userController.js";
 import { postController } from "./post/postController.js";
 import { authController } from "./auth/authController.js";
 import { likeController } from "./like/likeController.js";
-import { isAuthenticated } from "./auth/isAuthenticated.js";
 import { uploadController } from "./upload/uploadController.js";
 
 const routes = Router();
@@ -11,7 +10,6 @@ export const apiRoutes = routes;
 
 routes.post("/login", authController);
 
-// routes.use(isAuthenticated);
 routes.use("/users", userRouter);
 routes.use("/posts", postController);
 routes.use("/likes", likeController);
