62b665852104f8c7db249b2288d7b8c8b577004c
Ulisses <iamurise@gmail.com>
2023-01-31 07:01:45 -0300

    add post page user link

--- src/post/getPostById.js
+++ src/post/getPostById.js
@@ -3,6 +3,7 @@ import { db } from "../database/db.js";
 export async function getPost(id) {
   const post = await db.post.findUniqueOrThrow({
     where: { id },
+    include: { user: true },
   });
   return post;
 }
