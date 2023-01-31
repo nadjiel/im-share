dd9982631699fff36340040769cd588a2825ab16
Ulisses <iamurise@gmail.com>
2023-01-31 10:01:14 -0300

    add comments list

--- src/post/getPostById.js
+++ src/post/getPostById.js
@@ -3,7 +3,10 @@ import { db } from "../database/db.js";
 export async function getPost(id) {
   const post = await db.post.findUniqueOrThrow({
     where: { id },
-    include: { user: true },
+    include: {
+      user: true,
+      comments: { include: { user: true } },
+    },
   });
   return post;
 }
