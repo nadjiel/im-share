e14a9dc623ed2276efafff1c2b52cfc121d2e1ce
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-10-27 21:38:02 -0300

    add env example

--- src/env.js
+++ src/env.js
@@ -10,10 +10,8 @@ export const {
   DATABASE_URL,
 
   CLOUDINARY_SECRET,
-  // secure to share
-  CLOUDINARY_NAME = "dlwoimstk",
-  CLOUDINARY_API_KEY = "378278351497316",
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
 
-  // secure to share
   GOOGLE_CLIENT_ID = "372766069915-ca48cmscjcbehq752nrqtl0blfh05esk.apps.googleusercontent.com",
 } = process.env;
